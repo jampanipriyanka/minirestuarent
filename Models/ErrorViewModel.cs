8f20d8aac0af288f4627f060f0c3f3bf9d5765d5
jampanipriyanka <jpriyanka2023@gmail.com>
2025-04-14 17:35:11 -0400

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace minirestuarent.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
